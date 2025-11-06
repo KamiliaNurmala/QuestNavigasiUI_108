72f7607d1d466cbee3103f5780e2d97d39aaa48e
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-11-06 15:59:10 +0700

    Menambahkan HorizontalDivider

--- app/src/main/java/com/example/praktikum6/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum6/view/Formulir.kt
@@ -45,7 +45,10 @@ fun FormIsian(
                 label = {Text(text = "Nama Lengkap",
                     onValueChange = {},
                 )
-
+                    HorizontalDivider(modifier = Modifier
+                        .padding(20.dp)
+                        .width(250.dp), thickness = Thickness, color = Color.Red)
+                    
                 }
             )
         }
